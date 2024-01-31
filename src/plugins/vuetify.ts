3f55018e3ee2e6c7812b97a6249b2586f33b2186
Alexander Levin <alevin.ru@gmail.com>
2024-01-31 13:52:49 +0200

    icons mdi

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,18 @@
 // import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { mdi } from 'vuetify/iconsets/mdi-svg'
+import { mdiArrowLeft } from '@mdi/js'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases: { mdiArrowLeft },
+    sets: {
+      mdi,
+    },
+  },
   theme: {
     themes: {
       light: {
